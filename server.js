a7653d2980922c58ca9caabf5bdf80bc2b2c66d6
Oka R. Abdillah <hans.abdillh05@gmail.com>
2021-12-14 15:08:42 +0800

    Create dynamic PORT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const connection = require('./connection');
 const router = require('./router');
 
 //Setup Service
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 //Use modules
