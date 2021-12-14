129ca1811856b6c9a1d76525637ce7526d02d8d2
Oka R. Abdillah <hans.abdillh05@gmail.com>
2021-12-14 20:15:37 +0800

    Create config for disable cors

--- server.js
+++ server.js
@@ -14,7 +14,19 @@ const app = express();
 //Use modules
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+
+const allowedOrigins = ["https://evote-himati.vercel-app"];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}));
 
 //Router
 router(app);
